387869ca43985fc9b8647d0a0982a28f2ba3b94a
kyungminkim11 <66110823+kyungminkim11@users.noreply.github.com>
2026-06-28 14:32:01 +0900

    Add TimeFit service configuration

---
+++ js/config.js
@@ -0,0 +1,4 @@
+export const SUPABASE_URL = 'https://jnciddblcndmthmmvqrz.supabase.co';
+export const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_UUzSE7O9wqI0WN9cKG9OAQ_VleRkL4I';
+export const KAKAO_JS_KEY = '9dd3396dc9afd1f1fe1b0cc2bbc42096';
+export const SITE_URL = 'https://timefit.lavalabs.co.kr';
